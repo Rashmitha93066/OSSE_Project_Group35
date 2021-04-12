1f786685aba7dcf80f73222cde218b13b9296139
rahul-reji <79362576+rahul-reji@users.noreply.github.com>
2021-04-12 22:37:42 +0400

    Add files via upload
    Adding the Main.py file.

---
+++ Main.py
@@ -0,0 +1,27 @@
+#include files to be added here
+
+while(True):
+    print("Hello User! What would you like to do today? -\n" \
+            "1. Calculator\n" \
+            "2. Calculate Area of plane figures\n" \
+            "3. Multiply\n" \
+            "4. Exit\n")
+    
+    
+    # Take input from the user 
+    select = int(input("Select operations form 1, 2, 3, 4 : "))
+    
+    if select == 1:
+        print(1)
+        # Calculator Function
+    
+    elif select == 2:
+        print(2)
+        # Rashmita function
+    
+    elif select == 3:
+        print(2)
+        # Raj function
+
+    elif select == 4:
+        quit()
