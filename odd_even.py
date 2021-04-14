adf24a22186ace858bb116819f3590ceca75481b
rajkishore-bits <79572856+rajkishore-bits@users.noreply.github.com>
2021-04-14 15:42:05 +0530

    function to find odd even number

--- odd_even.py
+++ odd_even.py
@@ -1,5 +1,5 @@
 # Program to check if the input number is odd or even.
-
+# Take your input
 def find(num):
     # code logic here
     if num%2 == 0:
@@ -8,6 +8,6 @@ def find(num):
         numtype = "odd"
     return numtype
 
-num = int(input('Enter the number: '))  # 1. take your input
+num = int(input('Enter the number: '))  # 
 numtype = find(num)                     # 2. call the find function
 print('Given number is',numtype).       # 3. print if the number is even or odd
